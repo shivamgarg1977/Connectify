881136ced98304969c188f78bffbe36359b65bf3
Shivam Garg <shivamgarg1977@gmail.com>
2023-05-21 23:35:52 +0530

    Initial commit

---
+++ app/src/main/java/com/shivamgarg1977/connectify/MainActivity.java
@@ -0,0 +1,14 @@
+package com.shivamgarg1977.connectify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
